862e73833b53cc49b3899f6ebf947b36fed78499
Michal Svačina <msvacina@outlook.com>
2020-12-20 14:35:47 +0100

    Set up logging

--- src/app.py
+++ src/app.py
@@ -1,18 +1,29 @@
 import configparser
 import json
+import logging
+
 
 def main():
-    config = loadConfig('src/config.json')
+    config = loadConfig("src/config.json")
+    logName = config["LogName"] + ".log"
+    logging.basicConfig(filename=logName, filemode="w+", level=logging.DEBUG)
 
-    sleepTime = config['SleepTime']
-    print(sleepTime)
+    logging.info("Program started, config loaded")
+
+    sleepTime = config["SleepTime"]
+    logging.info('Sleeptime set to %s', sleepTime)
+
+    while 1:
+        print('lol')
+
 
 def loadConfig(configName):
     config = configparser.ConfigParser()
 
-    with open(configName, 'r') as f:
+    with open(configName, "r") as f:
         config = json.load(f)
 
     return config
+
 
 main()
